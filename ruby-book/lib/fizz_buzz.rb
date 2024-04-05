d7905e7bcc3f9f79418b849a9ec53465e63d29b0
Yusuke0620 <boston19851229@gmail.com>
2024-04-06 00:03:17 +0900

    プロを目指すRuby入門#書籍 第2章　Rubyの基礎を理解するReading

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,26 +1,14 @@
+n = 10
+puts n.to_s
 
-# 3で割り切れる数値を引数に渡すと,Fizzを返す
-# 5ならBuzz
-# 15ならFizz Buzz
-# それ以外ならその数値を文字列に変えて返す
-def fizz_buzz(n)
-    if n % 15 == 0
-        'Fizz Buzz'
-    elsif n % 3 == 0
-        'Fizz'
-    elsif n % 5 == 0
-        'Buzz'
-    else
-    n.to_s
-    end
 
+{ twitter: 45, name: 'Yusuke' }
+
+
+
+
+class Person
+  attr_accessor :name
+  attr_accessor :price
 end
 
-# 1.upto(50) do |n|
-#     puts fizz_buzz(n)
-# end
-
-fruits = ['apple', 'banana', 'cherry']
-p fruits.sample
-
-p fruits
