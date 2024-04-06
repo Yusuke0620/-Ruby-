ba98d687fa83965366c037da7e8a18d71e761dae
Yusuke0620 <boston19851229@gmail.com>
2024-04-06 15:06:19 +0900

    testfile add

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,14 +1,28 @@
-n = 10
-puts n.to_s
-
-
-{ twitter: 45, name: 'Yusuke' }
-
-
-
-
-class Person
-  attr_accessor :name
-  attr_accessor :price
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
 end
+
+
+require 'minitest/autorun'
+
+class FizzBuzzTEST < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+  end
+end
+
+# 1.upto(30) do |n|
+#   puts fizz_buzz(n)
+# end
+
 
